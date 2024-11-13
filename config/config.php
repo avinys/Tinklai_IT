d6ff49a4e3fdfe786e10ef414a51d7f5201b1092
Arvydas <arvydas.vingis1@gmail.com>
2024-11-14 01:50:27 +0200

    Fixed db credentials

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'tinklaiit');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'stud');
+define('DB_PASS', 'stud');
 
 define('BASE_URL', 'http://localhost/projektas'); // PAKEISTI PRIES LEIDZIANT VM
 
